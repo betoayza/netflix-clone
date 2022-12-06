52e192b464be43b9e31fa14a60dc2c305bb82abd
betoayza <aaayza@gmail.com>
2022-12-05 22:20:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { NetflixClone } from "./components/NetflixClone";
+
+function App() {
+  return (
+    <div className="App h-auto">
+      <NetflixClone />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NetflixClone.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NetflixClone = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
