ffd32fbf796719e54f223c5127077373f1caa220
betoayza <aaayza@gmail.com>
2023-01-27 20:45:00 -0300

    Finished

--- src/components/pure/Comment.jsx
+++ src/components/pure/Comment.jsx
@@ -2,14 +2,7 @@ import React from "react";
 
 export const Comment = ({ comment }) => {
   return (
-    <div
-      className={""}
-      style={{
-        border: "3px solid",
-        borderColor: "#800080",
-        borderRadius: "10px",
-      }}
-    >
+    <div>
       <h6 style={{ color: "#c32148" }}>@{comment.user.username}</h6>
       <p>{comment.comment}</p>
     </div>
